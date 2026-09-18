0d4b03a53b21df35c05970aa69fdad2dda927177
Its-ekjot <itsekjot@gmail.com>
2026-09-18 16:48:55 -0400

    Update Terraform state file path in backend configuration

--- terraform.tf
+++ terraform.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pragra-sep17"
-    key    = "env/dev/terraform.tfstate"
+    key    = key = "env/dev/app/terraform.tfstate"
     region = "ca-central-1"
   }
 }
